8106b75adc3b6564a171639afbf5a563f160ee4d
Tommymarvel <ibironketomiwa4@gmail.com>
2025-07-05 12:38:20 +0100

    ads

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,12 +1,13 @@
 const csp = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' *.vercel-insights.com https://www.googletagmanager.com https://www.google-analytics.com https://pagead2.googlesyndication.com https://adservice.google.com https://securepubads.g.doubleclick.net https://www.gstatic.com https://ep2.adtrafficquality.google; frame-src 'self' https://*.googlesyndication.com https://*.youtube.com;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' *.vercel-insights.com https://www.googletagmanager.com https://www.google-analytics.com https://pagead2.googlesyndication.com https://adservice.google.com https://securepubads.g.doubleclick.net https://www.gstatic.com https://ep2.adtrafficquality.google;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.gstatic.com;
   img-src * data: blob: https://www.google-analytics.com https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://www.googletagmanager.com https://www.gstatic.com https://tpc.googlesyndication.com;
   font-src 'self' data: https://fonts.gstatic.com;
   connect-src * https://www.google-analytics.com https://region1.google-analytics.com https://stats.g.doubleclick.net https://pagead2.googlesyndication.com https://adservice.google.com https://securepubads.g.doubleclick.net;
+  frame-src 'self' https://*.scoreswift.in https://googleads.g.doubleclick.net https://tpc.googlesyndication.com https://www.youtube.com;
   frame-ancestors 'self';
-  frame-src https://*.scoreswift.in https://googleads.g.doubleclick.net https://tpc.googlesyndication.com https://www.youtube.com;  object-src 'none';
+  object-src 'none';
   base-uri 'self';
 `
   .replace(/\s{2,}/g, ' ')
