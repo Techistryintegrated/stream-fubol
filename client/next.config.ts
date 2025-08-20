fd72d5917e9b00e631be6e828f4f0f5a0ed5531a
Abdulazeez Ojulari <jlrabdulazeez@gmail.com>
2025-08-20 09:27:31 +0100

    fix: meta pixel

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,7 @@
 const csp = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' *.vercel-insights.com https://www.googletagmanager.com https://www.google-analytics.com https://pagead2.googlesyndication.com https://adservice.google.com https://securepubads.g.doubleclick.net https://www.gstatic.com https://ep2.adtrafficquality.google;
+  script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' *.vercel-insights.com https://www.googletagmanager.com https://www.google-analytics.com https://pagead2.googlesyndication.com https://adservice.google.com https://securepubads.g.doubleclick.net https://www.gstatic.com https://ep2.adtrafficquality.google https://connect.facebook.net/en_US/fbevents.js https://www.facebook.com/tr?id=2587638381589796&ev=PageView&noscript=1;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' *.vercel-insights.com https://www.googletagmanager.com https://www.google-analytics.com https://pagead2.googlesyndication.com https://adservice.google.com https://securepubads.g.doubleclick.net https://www.gstatic.com https://ep2.adtrafficquality.google https://connect.facebook.net/en_US/fbevents.js https://www.facebook.com/tr?id=2587638381589796&ev=PageView&noscript=1;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.gstatic.com;
   img-src * data: blob: https://www.google-analytics.com https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://www.googletagmanager.com https://www.gstatic.com https://tpc.googlesyndication.com;
   font-src 'self' data: https://fonts.gstatic.com;
@@ -22,10 +23,10 @@ const securityHeaders = [
     key: 'X-Content-Type-Options',
     value: 'nosniff',
   },
-  {
-    key: 'Content-Security-Policy',
-    value: csp,
-  },
+  // {
+  //   key: 'Content-Security-Policy',
+  //   value: csp,
+  // },
 ];
 
 const nextConfig = {
